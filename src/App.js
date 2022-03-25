446fd368c209e3aed619a558983302dd1da05410
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-03-25 18:59:36 +0530

    Did basic route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import "./App.css";
-import {Home} from "./Pages/Index"
+import { Home } from "./Pages/Index";
+import { Routes, Route, useLocation } from "react-router-dom";
+import { Explore,Login,  } from "./Pages/Index";
+import {Topbar, Sidebar} from "./Component/index"
+
 
 function App() {
+
+  const {pathname} = useLocation() 
+  
+
   return (
     <div className="app-container">
-       <Home />
+      {pathname !== "/" && <Topbar />}
+      {/* {pathname !== "/" && <Sidebar />} */}
+      <Routes>
+        <Route path="/" element={<Home />} ></Route>
+        <Route path="/explore" element={<Explore />}></Route>
+        <Route path="/login" element={<Login />} > </Route>
+      </Routes>
     </div>
-  );
+  ); 
 }
 
 export default App;
