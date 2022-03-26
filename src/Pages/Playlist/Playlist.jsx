dcd62aaec18fb6bc81fb82694de55fa42bb17126
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-03-26 15:49:08 +0530

    Playlist component

---
+++ src/Pages/Playlist/Playlist.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Playlist.css"
+
+export const Playlist = () => {
+    return (
+        <>
+         <div className="main-container playlist-container">
+            <h1>Playlist</h1>
+         </div>
+        </>
+    )
+}
