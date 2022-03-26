d69805111848221f415611e554f1e7f55dff2f31
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-03-26 15:50:36 +0530

    Mockman component for API testing

---
+++ src/Component/Mockman/Mockman.jsx
@@ -0,0 +1,12 @@
+
+import React from "react";
+import Mockman from "mockman-js";
+function MockAPI() {
+  return (
+    <div className="MockAPI main-container" style={{marginLeft: "0"}}>
+      <Mockman />
+    </div>
+  );
+}
+
+export  {MockAPI};
