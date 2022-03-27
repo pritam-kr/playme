a2c41adc7b4527b358d623d09b534051facd75d3
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-03-27 12:21:56 +0530

    barrel export

--- src/Context/Index.js
+++ src/Context/Index.js
@@ -1,2 +1,3 @@
 export {VideoContextProvider, useVideoContext} from "./VideoContext"
 export {useAuthContext, AuthContextProvider} from "./AuthContext"
+export {LikesContextProvider, useLikesContext} from "./LikesContext"
