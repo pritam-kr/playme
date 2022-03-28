3b3c24fbd0d64257257f28824391c89a6b7442fe
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-03-28 11:10:58 +0530

    provided likes and history context

--- src/index.js
+++ src/index.js
@@ -3,19 +3,26 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { makeServer } from "./server";
 import { BrowserRouter } from "react-router-dom";
-import { VideoContextProvider,AuthContextProvider,LikesContextProvider } from "./Context/Index";
+import {
+  VideoContextProvider,
+  AuthContextProvider,
+  LikesContextProvider,
+  HistoryContextProvider,
+} from "./Context/Index";
 // Call make Server
 makeServer();
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-    <AuthContextProvider>
-      <VideoContextProvider>
-        <LikesContextProvider> 
-        <App />
-        </LikesContextProvider>
-      </VideoContextProvider>
+      <AuthContextProvider>
+        <VideoContextProvider>
+          <LikesContextProvider>
+            <HistoryContextProvider>
+              <App />
+            </HistoryContextProvider>
+          </LikesContextProvider>
+        </VideoContextProvider>
       </AuthContextProvider>
     </BrowserRouter>
   </React.StrictMode>,
