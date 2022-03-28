fae6cb28d3009b5147961816b71d7b8583d68a7d
Pritam Kumar <84632214+pritam-kr@users.noreply.github.com>
2022-03-28 11:11:31 +0530

    code formatted

--- src/Component/Sidebar/Sidebar.jsx
+++ src/Component/Sidebar/Sidebar.jsx
@@ -1,24 +1,44 @@
-import React from "react"
-import "./Sidebar.css"
+import React from "react";
+import "./Sidebar.css";
 import * as FaIcons from "react-icons/fa";
-import * as BiIcons from "react-icons/bi";
+import { useNavigate } from "react-router-dom";
+import { useAuthContext } from "../../Context/Index";
 
 const Sidebar = () => {
-    return(
-        <>
-        <div className="sidebar-wrapper">
-            <ul className="sidebar-links-wrapper">
-                <li className="sidebar-links sidebar-home text-lg"><FaIcons.FaHome className="icons sidebar-icons"/> Home </li>
-                <li className="sidebar-links sidebar-explore text-lg"><FaIcons.FaCompass className="icons sidebar-icons"/> Explore</li>
-                <li className="sidebar-links sidebar-playlist text-lg"><FaIcons.FaFolder className="icons sidebar-icons"/> Playlist</li>
-                <li className="sidebar-links sidebar-likes text-lg"><FaIcons.FaThumbsUp className="icons sidebar-icons"/> Likes</li>
-                <li className="sidebar-links sidebar-watch-later text-lg"><FaIcons.FaArchive className="icons sidebar-icons"/> Watch Later</li>
-                <li className="sidebar-links sidebar-history text-lg"><FaIcons.FaStopwatch className="icons sidebar-icons"/> History</li>
-                <li className="sidebar-links sidebar-logout text-lg logout-list"><FaIcons.FaUserCircle className="icons sidebar-icons"/> Logout</li>
-            </ul>
-        </div>
-        </>
-    )
-}
+  const { isAuth } = useAuthContext();
+  const navigate = useNavigate()
 
-export {Sidebar}
+  return (
+    <>
+      <div className="sidebar-wrapper">
+        <ul className="sidebar-links-wrapper">
+          <li className="sidebar-links sidebar-home text-lg">
+            <FaIcons.FaHome className="icons sidebar-icons" /> Home 
+          </li>
+          <li className="sidebar-links sidebar-explore text-lg" onClick={() =>  navigate('/explore')}>
+            <FaIcons.FaCompass className="icons sidebar-icons" /> Explore
+          </li>
+          <li className="sidebar-links sidebar-playlist text-lg">
+            <FaIcons.FaFolder className="icons sidebar-icons" /> Playlist
+          </li>
+
+         <li className="sidebar-links sidebar-likes text-lg" onClick={() => {!isAuth ? navigate('/login'): navigate("/likes")}}>
+            <FaIcons.FaThumbsUp className="icons sidebar-icons" /> Likes
+          </li>
+
+          <li className="sidebar-links sidebar-watch-later text-lg">
+            <FaIcons.FaClock className="icons sidebar-icons" /> Watch Later
+          </li>
+          <li className="sidebar-links sidebar-history text-lg" onClick={() => {!isAuth ? navigate('/login'): navigate("/history")}}>
+            <FaIcons.FaHistory className="icons sidebar-icons" /> History
+          </li>
+          <li className="sidebar-links sidebar-logout text-lg logout-list">
+            <FaIcons.FaUserCircle className="icons sidebar-icons" /> Logout
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export { Sidebar };
